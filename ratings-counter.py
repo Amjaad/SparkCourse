0114b674ee27f5a2cba1fe26995f87b2ccba2021
amjaad <amjad.alharbi@colorado.edu>
2017-01-11 10:17:58 -0700

    Added course materials

---
+++ ratings-counter.py
@@ -0,0 +1,16 @@
+import  os
+#check if pyspark env vars are set and then reset to required or delete.
+del os.environ['PYSPARK_SUBMIT_ARGS']
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
